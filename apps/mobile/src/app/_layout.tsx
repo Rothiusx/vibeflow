8505bced93fe67fec40642683dc27b39fa5dd740
Adam Molin <Rothiusx@seznam.cz>
2025-04-27 00:37:55 +0200

    Added NativeWind to the Expo app

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -11,6 +11,7 @@ import * as SplashScreen from 'expo-splash-screen'
 import { StatusBar } from 'expo-status-bar'
 import { useEffect } from 'react'
 import 'react-native-reanimated'
+import '@/globals.css'
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync()
